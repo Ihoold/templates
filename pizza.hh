0b8a1b55ed57ec32146f8c5aa29a4c2b81bfc435
Michał Waszczuk <mw361260@students.mimuw.edu.pl>
2015-12-08 14:33:37 +0100

    zrobione wyliczanie optymalnej liczby kawalkow, drobna zmiana w koncepcji (pizza przechowuje informacje o pizzeri w ktorej zostala zrobiona)

--- pizza.hh
+++ pizza.hh
@@ -20,18 +20,6 @@ constexpr int NumberOfType(int pos)
   return (std::is_same<LookingFor,Head>::value) ? pos : NumberOfType<LookingFor,Tail...>(pos+1);
 }
 
-/*
-template<bool B, size_t t, size_t f>
-struct transfer() {static constexpr size_t impl() {return t;}};
- 
-template<size_t t, size_t f>
-struct transfer<false, t, f>() {static constexpr size_t impl() {return t;}};
-
-template<bool B, size_t t, size_t f>
-constexpr size_t transform(){return transfer<B,t,f>::impl();}
-*/
-
-
 template <typename... List>
 struct IsInjective;
 //IsInjective<List...>::value is true iff there are no two same types in List
@@ -46,10 +34,33 @@ struct IsInjective <T> {
 	enum { value = true };
 };
 
+// Returns number of pieces to take
+// takes: max and curr are part of recursive call
+// amount is maximum of pieces we can take of given type
+template <typename Kind>
+constexpr int most_yummy (int max, size_t curr, size_t amount)
+{
+  return (curr == amount+1) ? max :
+    ((Kind::yumminess(curr) >= max) ? 
+     most_yummy<Kind>(Kind::yumminess(curr), curr+1,amount):
+     most_yummy<Kind>(max, curr+1, amount));
+}
+
+
 //\HELPERS
+template<typename Pizza1, typename Pizza2> 
+struct best_mix
+{
+  static_assert(std::is_same<typename Pizza1::type, typename Pizza2::type>::value,
+		"We can't mix pizzas from diffrent pizzerias!");
+  //  typedef Pizza<Pizza1::type,> type;
+};
 
 template<typename... Kinds> struct Pizzeria {
-  template<size_t... slices> struct Pizza {
+  static_assert(IsInjective<Kinds...>::value,
+  		"No need to mention one pizza type twice!");
+  template<typename Pizza_type, size_t... slices> 
+  struct Pizza {
   public:
     static constexpr std::array<size_t, sizeof... (Kinds)> as_array() {
       const std::array<size_t, sizeof... (slices)> arr_of_types = {{slices...}};
@@ -60,16 +71,18 @@ template<typename... Kinds> struct Pizzeria {
       constexpr std::array<size_t, sizeof... (slices)> arr_of_types = {{slices...}};
       return arr_of_types[NumberOfType<PKind,Kinds...>(0)];
     }
+  private:
+    typedef Pizzeria<Kinds...> type;
+  public:
+    typedef Pizza<type,(2*slices)...> sliced_type;
 
-    typedef Pizza<(2*slices)...> sliced_type;
   };
 
   template<typename Kind> struct make_pizza {
-  static_assert(IsInjective<Kinds...>::value,
-  		"No need to mention two pizza types twice!");
-    static_assert(NumberOfType<Kind, Kinds...>(0) != -1);
+    static_assert(NumberOfType<Kind, Kinds...>(0) != -1,
+		  "We don't have this one in menu!");
   public:
-    typedef Pizza<(std::conditional<
+    typedef Pizza<Pizzeria, (std::conditional<
 		   std::is_same<Kinds,Kind>::value,
 		   std::integral_constant<size_t,8>,
 		   std::integral_constant<size_t,0> 
